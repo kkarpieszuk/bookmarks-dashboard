27b46b234dd0d01b3a19e6475f495e997ca9ed21
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2020-03-15 00:13:15 +0100

    key for each bookmark element

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Bookmark from '../Bookmark';
 
 const Dashboard = ( {bookmarks} ) => (<div className='dashboard'>
-        {bookmarks.map( el => <Bookmark bookmark={el} /> )}    
+        {bookmarks.map( el => <Bookmark bookmark={el} key={el.id} /> )}    
     </div>
     )
 
